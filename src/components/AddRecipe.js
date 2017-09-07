3d8cc3ed93d137c8cb783538ff28e39df786edb3
ashleydonohoe <ashleyrdonohoe@gmail.com>
2017-09-07 00:15:13 -0400

    working on static model of recipe items

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -5,7 +5,7 @@ class AddRecipe extends Component {
     return (
       <div>
         <button className="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">Add Recipe</button>
-        <div className="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div className="modal fade" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
           <div className="modal-dialog" role="document">
             <div className="modal-content">
               <div className="modal-header">
@@ -13,7 +13,16 @@ class AddRecipe extends Component {
                 <h4 className="modal-title" id="myModalLabel">Add a Recipe</h4>
               </div>
               <div className="modal-body">
-                ...
+              <form>
+                <div className="form-group">
+                  <label>Recipe</label>
+                  <input type="text" className="form-control" id="recipe-name" aria-describedby="recipe-name" placeholder="Recipe Name"/>
+                </div>
+                <div className="form-group">
+                  <label>Ingredients</label>
+                  <input type="text" className="form-control ingredients-input" id="ingredients" placeholder="Enter ingredients, separated by commas"/>
+                </div>
+              </form>
               </div>
               <div className="modal-footer">
                 <button type="button" className="btn btn-primary">Add Recipe</button>
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,29 +1,11 @@
 import React from 'react';
+import RecipeItem from "./RecipeItem";
 
 class RecipeList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     const recipes = this.props.recipes;
     return (
-      <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-          <div className="panel panel-default">
-              <div className="panel-heading" role="tab" id="headingOne">
-                  <h4 className="panel-title">
-                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                      Collapsible Group Item #1
-                    </a>
-                  </h4>
-              </div>
-              <div id="collapseOne" className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-                  <div className="panel-body">
-                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                  </div>
-              </div>
-          </div>
-      </div>
+      <RecipeItem recipe={this.props.recipes[0]}/>
     )
   }
 };
