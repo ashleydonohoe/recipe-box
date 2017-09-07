6687ef591c45b38785c462fe3a77097dfea48f63
ashleydonohoe <ashleyrdonohoe@gmail.com>
2017-09-06 23:28:37 -0400

    working on initial layout

---
+++ src/Recipes.js
@@ -0,0 +1,30 @@
+let recipes = [
+  {
+    name: "Pumpkin Pie",
+    ingredients: [
+      "Pumpkin Puree",
+      "Sweetened Condensed Milk",
+      "Eggs",
+      "Pumpkin Pie Spice",
+      "Pie Crust"
+    ]
+  },
+  {
+    name: "Spaghetti",
+    ingredients: [
+      "Noodles",
+      "Tomato Sauce",
+      "(Optional) Meatballs"
+    ]
+  },
+  {
+    name: "Onion Pie",
+    ingredients: [
+      "Onion",
+      "Pie Crust",
+      "Sounds Yummy Right?"
+    ]
+  }
+];
+
+export default recipes;
